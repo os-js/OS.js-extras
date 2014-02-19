013674c4e36e21de3dcc3219369dc917d281db6e
Anders Evenrud <andersevenrud@gmail.com>
2014-02-19 18:43:00 +0100

    Added missing file

---
+++ GUITest/api.php
@@ -0,0 +1,14 @@
+<?php
+
+class GUITest
+{
+  public static function call($method, $arguments) {
+    return Array(
+      "message" => "You called the Application API (backend)",
+      "requestMethod" => $method,
+      "requestArguments" => $arguments
+    );
+  }
+}
+
+?>
